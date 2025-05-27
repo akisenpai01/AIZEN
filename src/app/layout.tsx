e71454fc279e1e29ec147408b3fd6b8b43af77a6
Atiyab Rehman <atiyabatique2@gmail.com>
2025-05-27 15:00:38 +0000

    https://imagekit.io/tools/asset-public-link?detail=%7B%22name%22%3A%22wp9226062-4k-samurai-mobile-wallpapers.jpg%22%2C%22type%22%3A%22image%2Fjpeg%22%2C%22signedurl_expire%22%3A%222028-05-03T06%3A37%3A17.565Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F7e8c99534f4d4798%2Fwp9226062-4k-samurai-mobile-wallpapers.jpg%3FExpires%3D1840948638%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3Dr62I1Q4W4YHDy7eiAKqwWodbPFzVNXXwioylagBQi16o1rzM9Y6dcqUPdEi3RT-sYxwAiJdHM74NsQs-Uvb4lM7lUKxM9ZzFbZOMaz9rrmV04KHyqrugDwVIQTOC7C95kY90o42Gd1lmMUznk-27FKLdFA1w82wzZFl0NbXnLu6~E2IIDbm391RQqbef8~TLw2rIRWM6BG0Efgjh4T34zIsevcrRGcsj~LoNgNPR12Kuk6VotvanRGnuSwBoMXj7mAnxLPwAafsbgi~rUxv-mWElzAlUD90cerywZme6rtLYp5g9nQB7e-YWpa3poyalPdIrIB9A-0YyoP8pyWKyow__%22%7D make this image the background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <div 
           className="fixed inset-0 z-[-1] bg-cover bg-center bg-no-repeat"
-          style={{ backgroundImage: "url('https://placehold.co/1920x1080/232323/232323.png?text=.')" }}
-          data-ai-hint="samurai temple"
+          style={{ backgroundImage: "url('https://media-hosting.imagekit.io/7e8c99534f4d4798/wp9226062-4k-samurai-mobile-wallpapers.jpg?Expires=1840948638&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=r62I1Q4W4YHDy7eiAKqwWodbPFzVNXXwioylagBQi16o1rzM9Y6dcqUPdEi3RT-sYxwAiJdHM74NsQs-Uvb4lM7lUKxM9ZzFbZOMaz9rrmV04KHyqrugDwVIQTOC7C95kY90o42Gd1lmMUznk-27FKLdFA1w82wzZFl0NbXnLu6~E2IIDbm391RQqbef8~TLw2rIRWM6BG0Efgjh4T34zIsevcrRGcsj~LoNgNPR12Kuk6VotvanRGnuSwBoMXj7mAnxLPwAafsbgi~rUxv-mWElzAlUD90cerywZme6rtLYp5g9nQB7e-YWpa3poyalPdIrIB9A-0YyoP8pyWKyow__')" }}
+          data-ai-hint="samurai landscape"
           aria-hidden="true"
         />
         <div className="relative min-h-screen flex flex-col">
